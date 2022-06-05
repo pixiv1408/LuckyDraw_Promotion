1a60be0697d1b352eb84c25bb4361e44ced60392
Lê Thành <86051732+pixiv1408@users.noreply.github.com>
2022-06-05 16:33:34 +0700

    Update-Models-Database

---
+++ Luckydraw_Promotion/CMS_Lucky/CMS_Lucky/DataConnect.cs
@@ -0,0 +1,16 @@
+﻿using Datactx.Dbcontext;
+using Microsoft.EntityFrameworkCore;
+
+namespace CMS_Lucky
+{
+    public static class DataConnect
+    {
+        public static IServiceCollection ServicesCollection(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddDbContext<DatabaseContext>(options => options.UseSqlServer(configuration.GetConnectionString("DatabaseContext"),
+              x => x.MigrationsAssembly(typeof(DatabaseContext).Assembly.FullName)), ServiceLifetime.Transient);
+
+            return service;
+        }
+    }
+}
